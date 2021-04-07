6b7008f5c16e2cd22d865b0a6029ec9c5d930e92
kishorebraj8 <80564801+kishorebraj8@users.noreply.github.com>
2021-04-07 20:46:36 +0530

    Add files via upload

---
+++ Problem9d2TrueFalse.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Problem9d2TrueFalse {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the number of elements in array-");
+		int p = input.nextInt();
+		int[] arr = new int[p];
+		System.out.println("Enter the elements of array-");
+		for (int i = 0; i < p; i++) {
+			arr[i] = input.nextInt();
+		}
+		System.out.println("Enter the value of n : ");
+		int n = input.nextInt();
+		int flag = 0;
+		for (int i = 0; i < p; i++) {
+			for (int j = 0; j < p; j++) {
+
+				if (arr[i] + arr[j] == n) {
+					flag++;
+				}
+			}
+		}
+		if (flag > 0) {
+			System.out.println("true");
+		} else {
+			System.out.println("false");
+		}
+
+	}
+
+}
