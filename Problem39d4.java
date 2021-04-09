105b69e69598ef88efc41bbd5214d765cfac0345
kishorebraj8 <80564801+kishorebraj8@users.noreply.github.com>
2021-04-09 12:37:42 +0530

    Add files via upload

---
+++ Problem39d4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Problem39d4 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the number of elements: ");
+		int a = input.nextInt();
+		System.out.println("Enter the elements of array: ");
+		int[] arr = new int[a];
+		for (int i = 0; i < a; i++) {
+			arr[i] = input.nextInt();
+		}
+		int max = Integer.MIN_VALUE, e = 0;
+		for (int i = 0; i < a; i++) {
+			e = e + arr[i];
+			if (max < e) {
+				max = e;
+			}
+			if (e < 0) {
+				e = 0;
+			}
+		}
+		System.out.println(max);
+	}
+
+}
